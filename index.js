3b545c1fd4bdc1c4e57aa124c307d9c6c2b3b634
Michael Chung <mikechung89@gmail.com>
2022-02-22 15:05:54 -0500

    commit tree 73545937d129816eacb61600606e6d2808bcaa29

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(st) {
+    const blocks = 42 - st;
+    if (blocks < 0) {
+        return blocks * -1;
+    } else return blocks
+}
+
+function distanceFromHqInFeet(st) {
+    return distanceFromHqInBlocks(st) * 264;
+}
+
+function distanceTravelledInFeet(from, to) {
+    const blocks = Math.abs(from - to);
+    return blocks * 264;
+}
+
+function calculatesFarePrice(from, to) {
+    if (distanceTravelledInFeet(from, to) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(from, to) > 400 && distanceTravelledInFeet(from, to) < 2000) {
+        return (distanceTravelledInFeet(from, to) - 400) * 0.02;
+    } else if (distanceTravelledInFeet(from, to) > 2000 && distanceTravelledInFeet(from, to) < 2500) {
+        return 25;
+    } else return "cannot travel that far"
+}
